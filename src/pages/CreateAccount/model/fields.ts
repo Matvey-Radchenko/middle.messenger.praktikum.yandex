54a38c730c527fe56c9df46096f4789af1d4f3ff
Матвей Радченко <MVRadchenko@alfabank.ru>
2024-12-12 02:48:37 +0100

    typecheck

--- src/pages/CreateAccount/model/fields.ts
+++ src/pages/CreateAccount/model/fields.ts
@@ -1,11 +1,7 @@
+import { User } from '@entities';
 import { TextInputProps } from '@shared';
-import { CreateAccountFormData } from './types/CreateAccountFormData';
 
-type CreateAccountFormDataKeys = keyof CreateAccountFormData;
-
-export const CREATE_ACCOUNT_FIELDS: Array<
-    TextInputProps & { name: CreateAccountFormDataKeys }
-> = [
+export const CREATE_ACCOUNT_FIELDS: Array<TextInputProps & { name: keyof User }> = [
     {
         name: 'email',
         type: 'email',
@@ -25,15 +21,5 @@ export const CREATE_ACCOUNT_FIELDS: Array<
         name: 'second_name',
         type: 'text',
         placeholder: 'Фамилия',
-    },
-    {
-        name: 'password',
-        type: 'password',
-        placeholder: 'Пароль',
-    },
-    {
-        name: 'passwordRepeat',
-        type: 'password',
-        placeholder: 'Пароль (ещё раз)',
     },
 ];
